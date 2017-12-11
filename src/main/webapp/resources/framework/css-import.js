0cd85c7395b09037040a80f1453764641b941c1a
BinaryLie <1499230032@qq.com>
2017-12-11 08:52:32 +0800

    bage备忘录2、css-import项目名改用js获取

--- src/main/webapp/resources/framework/css-import.js
+++ src/main/webapp/resources/framework/css-import.js
@@ -1,20 +1,31 @@
 /**
  * 引入公共的css样式
  */
-var contextPath = 'bbj-web';
+var _rootPath = (function() {
+    var path = location.pathname;
+
+    if (path.indexOf('/') === 0) {
+        path = path.substring(1);
+    }
+
+    return '/' + path.split('/')[0];
+}());
+
+var contextPath = _rootPath;
+
 document.writeln('<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">');
-document.writeln('<link rel="stylesheet" href="/'+contextPath+'/resources/bower_components/bootstrap/dist/css/bootstrap.min.css">');
-document.writeln('<link rel="stylesheet" href="/'+contextPath+'/resources/bower_components/font-awesome/css/font-awesome.min.css">');
-document.writeln('<link rel="stylesheet" href="/'+contextPath+'/resources/bower_components/Ionicons/css/ionicons.min.css">');
-document.writeln('<link rel="stylesheet" href="/'+contextPath+'/resources/dist/css/AdminLTE.css">');
-document.writeln('<link rel="stylesheet" href="/'+contextPath+'/resources/dist/css/skins/_all-skins.min.css">');
+document.writeln('<link rel="stylesheet" href="'+contextPath+'/resources/bower_components/bootstrap/dist/css/bootstrap.min.css">');
+document.writeln('<link rel="stylesheet" href="'+contextPath+'/resources/bower_components/font-awesome/css/font-awesome.min.css">');
+document.writeln('<link rel="stylesheet" href="'+contextPath+'/resources/bower_components/Ionicons/css/ionicons.min.css">');
+document.writeln('<link rel="stylesheet" href="'+contextPath+'/resources/dist/css/AdminLTE.css">');
+document.writeln('<link rel="stylesheet" href="'+contextPath+'/resources/dist/css/skins/_all-skins.min.css">');
 document.writeln('<!--[if lt IE 9]>');
 document.writeln('<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>');
 document.writeln('<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>');
 document.writeln('<![endif]-->');
 document.writeln('<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">');
 document.writeln('<!-- Pace style -->');
-document.writeln('<link rel="stylesheet" href="/'+contextPath+'/resources/bower_components/PACE/pace.min.js">');
+document.writeln('<link rel="stylesheet" href="'+contextPath+'/resources/bower_components/PACE/pace.min.js">');
     
 
 
